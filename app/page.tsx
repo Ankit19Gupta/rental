4d2bfc8737392f445d24b32623004d33c2f392c5
AnkitGupta <ankitgupta72950@gmail.com>
2025-12-18 21:56:21 +0530

    Lets Start The Journey

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="h-screen flex items-center justify-center flex-col">
       <h1>Page</h1>
+      <Button>Start Building</Button>
     </div>
   );
 };
